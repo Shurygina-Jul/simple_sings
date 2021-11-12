062267a089b6e6f28baa509f3025433fad23f2e5
Shurygina Julia <melam.2916@gmail.com>
2021-11-12 14:08:48 +0300

    Доработка слайдера

--- 01-cards-sources/app.js
+++ 01-cards-sources/app.js
@@ -1,14 +1,19 @@
-const slides = document.querySelectorAll(".slide");
+function slidesPlagin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide");
 
-slides.forEach((slide=>{
-    slide.addEventListener('click', function(){
-        clearActiveClass();
-        slide.classList.add('active')
-    })
-}))
+  slides[activeSlide].classList.add("active");
 
-function clearActiveClass(){
-    slides.forEach((slide)=>{
-slide.classList.remove('active')
-    })
+  slides.forEach((slide) => {
+    slide.addEventListener("click", function () {
+      clearActiveClass();
+      slide.classList.add("active");
+    });
+  });
+
+  function clearActiveClass() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
+slidesPlagin();
